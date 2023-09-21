e95fbbe14de809354aec9ad72ae08febbeb8d51f
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-09-21 11:10:55 +0900

    스크립트 수정

--- CatHero/Assets/Scripts/ItemSlotUI.cs
+++ CatHero/Assets/Scripts/ItemSlotUI.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ItemSlotUI : MonoBehaviour
+public class InventoryManager : MonoBehaviour
 {
     public Transform slotParent; // 슬롯들이 자식으로 들어갈 부모 Transform
-    public GameObject itemSlotPrefab; // 아이템 슬롯 프리팹
+    public GameObject ItemSlotPrefab; // 아이템 슬롯 프리팹
+    public GameObject IconPrefab;
     public Sprite[] itemSprites; // 아이템 스프라이트 배열
 
     private void Start()
@@ -14,8 +13,9 @@ public class ItemSlotUI : MonoBehaviour
         // 각각의 아이템 슬롯을 생성하고 이미지 설정
         for (int i = 0; i < itemSprites.Length; i++)
         {
-            GameObject itemSlot = Instantiate(itemSlotPrefab, slotParent);
-            Image slotImage = itemSlot.GetComponentInChildren<Image>(); // 슬롯 하위의 Image 컴포넌트를 찾음
+            GameObject itemSlot = Instantiate(ItemSlotPrefab, slotParent);
+            GameObject IconImg = Instantiate(IconPrefab, slotParent);
+            Image slotImage = IconImg.GetComponentInChildren<Image>(); // 슬롯 하위의 Image 컴포넌트를 찾음
             slotImage.sprite = itemSprites[i]; // 이미지 설정
         }
     }
