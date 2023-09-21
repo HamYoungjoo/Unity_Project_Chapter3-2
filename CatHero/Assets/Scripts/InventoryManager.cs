adb884e9fe766a834ed576414f5aea97efd2f30a
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-09-21 11:21:31 +0900

    스크립트 수정중

--- CatHero/Assets/Scripts/InventoryManager.cs
+++ CatHero/Assets/Scripts/InventoryManager.cs
@@ -3,17 +3,21 @@ using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    public Transform slotParent; // 슬롯들이 자식으로 들어갈 부모 Transform
-    public GameObject ItemSlotPrefab; // 아이템 슬롯 프리팹
+    public Transform slotParent; 
+    public GameObject ItemSlotBGPrefab; 
     public GameObject IconPrefab;
-    public Sprite[] itemSprites; // 아이템 스프라이트 배열
+    public Sprite[] itemSprites; 
 
     private void Start()
     {
-        // 각각의 아이템 슬롯을 생성하고 이미지 설정
+        for (int j = 0; j < 9;j++)
+        {
+            GameObject itemSlotBG = Instantiate(ItemSlotBGPrefab, slotParent);
+
+        }
+
         for (int i = 0; i < itemSprites.Length; i++)
         {
-            GameObject itemSlot = Instantiate(ItemSlotPrefab, slotParent);
             GameObject IconImg = Instantiate(IconPrefab, slotParent);
             Image slotImage = IconImg.GetComponentInChildren<Image>(); // 슬롯 하위의 Image 컴포넌트를 찾음
             slotImage.sprite = itemSprites[i]; // 이미지 설정
